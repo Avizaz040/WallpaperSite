2d26adb748ff3f3f7eeea5221e1ea51a874aec37
Avishek Mondal <avizaz040@gmail.com>
2025-07-11 02:18:11 +0530

    meta tag from google search console

--- src/app/layout.js
+++ src/app/layout.js
@@ -38,6 +38,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="google-site-verification" content="5hZCMM0nK63VTuvWOwQ7CTMbwTTMlN8DGBVP_eg2pIA" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${michroma.variable} antialiased`}
       >
