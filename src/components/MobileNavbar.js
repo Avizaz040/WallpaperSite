79cf12729d750fa1df6480fa174d3b51b06e6794
Avishek Mondal <avizaz040@gmail.com>
2025-07-14 17:22:39 +0530

    mobile navbar update 2

--- src/components/MobileNavbar.js
+++ src/components/MobileNavbar.js
@@ -1,29 +1,38 @@
+"use client"
+
 import React from 'react'
 import Link from 'next/link';
+import { usePathname } from 'next/navigation'; //
 import { IoHomeSharp, IoDesktopOutline } from "react-icons/io5";
 import { FaMobileAlt, FaRegUserCircle } from "react-icons/fa";
 
 
 function MobileNavbar() {
+  const pathname = usePathname();
+
+  const navItems = [
+    { href: '/', label: 'Home', icon: <IoHomeSharp /> },
+    { href: '/mobile_wallpapers', label: 'Mobile', icon: <FaMobileAlt /> },
+    { href: '/desktop_wallpapers', label: 'Desktop', icon: <IoDesktopOutline /> },
+    { href: '/login', label: 'Login', icon: <FaRegUserCircle /> }
+  ];
+
   return (
     <section className='fixed bottom-0 w-full px-2 py-2 lg:hidden flex items-center justify-around lg-hidden gap-6 text-white bg-teal-900 z-10'>
-      <Link className='flex flex-col items-center gap-2' href= "/">
-        <IoHomeSharp />
-        <span className='text-sm'>Home</span>
-      </Link>
-      <Link className='flex flex-col items-center gap-2' href= "/mobile_wallpapers">
-        <FaMobileAlt />
-        <span className='text-sm'>Mobile</span>
-      </Link>
-      <Link className='flex flex-col items-center gap-2' href= "/desktop_wallpapers">
-        <IoDesktopOutline />
-        <span className='text-sm'>Desktop</span>
-      </Link>
-      <Link className='flex flex-col items-center gap-2' href= "/login">
-        <FaRegUserCircle />
-        <span className='text-sm'>Login</span>
-      </Link>
-      
+      {navItems.map(({href,label,icon}) => {
+        const isActive = pathname === href;
+
+        return(
+          <Link
+          key={href}
+          href={href}
+          className={`flex flex-col items-center gap-1 text-sm ${isActive ? 'text-yellow-400 font-semibold' : 'text-white'}`}
+          >
+            {icon}
+            <span>{label}</span>
+          </Link>
+        );
+      })}
     </section>
   )
 }
