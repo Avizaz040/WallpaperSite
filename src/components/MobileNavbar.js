39474358a841ef794b99c4f399b33605328113a6
Avishek Mondal <avizaz040@gmail.com>
2025-07-14 17:04:32 +0530

    mobile menu update

--- src/components/MobileNavbar.js
+++ src/components/MobileNavbar.js
@@ -6,20 +6,20 @@ import { FaMobileAlt, FaRegUserCircle } from "react-icons/fa";
 
 function MobileNavbar() {
   return (
-    <section className='fixed bottom-0 w-full px-2 py-2 flex items-center justify-around lg-hidden gap-6 text-white bg-teal-900 z-10'>
-      <Link className='flex flex-col items-center' href= "/">
+    <section className='fixed bottom-0 w-full px-2 py-2 lg:hidden flex items-center justify-around lg-hidden gap-6 text-white bg-teal-900 z-10'>
+      <Link className='flex flex-col items-center gap-2' href= "/">
         <IoHomeSharp />
         <span className='text-sm'>Home</span>
       </Link>
-      <Link className='flex flex-col items-center' href= "/mobile_wallpapers">
+      <Link className='flex flex-col items-center gap-2' href= "/mobile_wallpapers">
         <FaMobileAlt />
         <span className='text-sm'>Mobile</span>
       </Link>
-      <Link className='flex flex-col items-center' href= "/desktop_wallpapers">
+      <Link className='flex flex-col items-center gap-2' href= "/desktop_wallpapers">
         <IoDesktopOutline />
         <span className='text-sm'>Desktop</span>
       </Link>
-      <Link className='flex flex-col items-center' href= "/login">
+      <Link className='flex flex-col items-center gap-2' href= "/login">
         <FaRegUserCircle />
         <span className='text-sm'>Login</span>
       </Link>
