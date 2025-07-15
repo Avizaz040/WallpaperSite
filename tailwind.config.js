1b8e762a31ffa0319c632f05329821eb4c6d35cb
Avishek Mondal <avizaz040@gmail.com>
2025-07-15 13:11:24 +0530

    not-found page bg update

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,10 @@ module.exports = {
         geist_mono: ['var(--font-geist-mono)', 'monospace'],
         // michroma: ['var(--font-michroma)', 'sans-serif'],
       },
+      backgroundImage: {
+        'main-gradient': 'linear-gradient(to right, black, #134e4a)', // teal-900 HEX
+      },
     },
   },
+  plugins: [],
 };
